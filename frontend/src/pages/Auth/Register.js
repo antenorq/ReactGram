c907fb1a93c609443d6207b592d25aeaea6405ff
antenorq <antenorq@gmail.com>
2023-04-10 23:57:46 -0400

    created pages + css home, register, login

--- frontend/src/pages/Auth/Register.js
+++ frontend/src/pages/Auth/Register.js
@@ -1,7 +1,32 @@
 import "./Auth.css";
 
+//components
+import { Link } from "react-router-dom";
+
+//hooks
+import { useState, useEffect } from "react";
+
 const Register = () => {
-  return <div>Register</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Register to see pictures of your friends</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Name" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <input type="password" placeholder="Confirm Password" />
+        <input type="submit" value="Register" />
+        <p>
+          Already have account? <Link to="/login">Click Here</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
